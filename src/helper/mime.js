4e423237cc6c4c938c22f45f80696c1e5c9d6b6f
bwlu <18232386357@163.com>
2019-07-13 20:56:39 +0800

    基础浏览功能

---
+++ src/helper/mime.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const mimeTypes = {
+  'css':'text/css',
+  'gif':'image/gif',
+  'png':'image/png',
+  'jpg':'image/jpeg',
+  'jpeg':'image/jpeg',
+  'json':'application/gif',
+  'js':'text/javascript',
+  'html':'text/html',
+  'xml':'text/xml'
+};
+
+module.exports = (filePath) => {
+  let ext = path.extname(filePath)
+    .split('.')//分割取最后一部分
+    .pop()
+    .toLowerCase();
+    if (!ext) {//读不到拓展名
+      ext = filePath;
+    }
+    return mimeTypes[ext] || mimeTypes['txt'];
+};
---
+++ src/helper/route.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const conf = require('../config/defaultConfig')
+const mime = require('./mime');
+
+const tplPath = path.join(__dirname,'../template/fileList.tpl')
+const source = fs.readFileSync(tplPath);
+const template = handlebars.compile(source.toString());
+
+module.exports = async function(req,res,filePath){
+  try {
+    const stats = await stat(filePath);
+    if (stats.isFile()) {
+      res.statusCode = 200;
+      const contentType = mime(filePath);
+      res.setHeader('Content-Type',contentType);
+      let rs = fs.createReadStream(filePath);
+      rs.pipe(res);
+    } else if (stats.isDirectory()) {
+      const files = await readdir(filePath);
+      res.statusCode = 200;
+      res.setHeader('Content-Type','text/html');
+      const dir = path.relative(conf.root,filePath);
+      res.end(template({
+        title:path.basename(filePath),
+        dir:dir ? `/${dir}`:'',
+        files
+      }));
+    }
+  } catch (ex) {
+    console.error(ex);
+    res.statusCode = 404;
+    res.setHeader('Content-Type','text/plain');
+    res.end(`${filePath} is not a directory or file.`);
+  }
+}
